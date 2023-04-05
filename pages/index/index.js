417f2ea8547f0e8e10111e361c8fe55d7846bd6b
qifengleSummer <b654820442@163.com>
2023-04-05 14:06:14 +0800

    项目初始化

---
+++ pages/index/index.js
@@ -0,0 +1,91 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+Page({
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
+    }
+  },
+
+  data: {
+    background: [
+      '../../assets/img/homePage/banner1.jpg',
+       '../../assets/img/homePage/banner2.jpg',
+        '../../assets/img/homePage/banner3.jpg'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+    categories: [],
+    nav2List: [{
+      id: '1',
+      name: '积分商城',
+      icon: "../../assets/img/homePage/integral.png"
+    },
+    {
+      id: '2',
+      name: '社群福利',
+      icon: "../../assets/img/homePage/welfare.png"
+    },
+    {
+      id: '3',
+      name: '团餐',
+      icon: "../../assets/img/homePage/groups.png"
+    },
+    {
+      id: '4',
+      name: '礼券兑换',
+      icon: "../../assets/img/homePage/ticket.png"
+    }]
+  },
+
+  onLoad() {
+    this.setData({
+      categories: [{
+        id: 1,
+        orderType: '自取',
+        desc: '即刻点单',
+        icon: '../../assets/img/homePage/dine-in.png'
+      },{
+        id: 2,
+        orderType: '外卖',
+        desc: '无接触配送',
+        icon: '../../assets/img/homePage/takeaway.png'
+      }]
+    });
+  },
+
+  // changeIndicatorDots() {
+  //   this.setData({
+  //     indicatorDots: !this.data.indicatorDots
+  //   })
+  // // },
+
+  // changeAutoplay() {
+  //   this.setData({
+  //     autoplay: !this.data.autoplay
+  //   })
+  // },
+
+  // intervalChange(e) {
+  //   this.setData({
+  //     interval: e.detail.value
+  //   })
+  // },
+
+  // durationChange(e) {
+  //   this.setData({
+  //     duration: e.detail.value
+  //   })
+  // }
+  tabClick(e) {
+   
+    
+  }
+})
+
+
+
